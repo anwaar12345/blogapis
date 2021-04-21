44b08fa133235c87e088032f3fd893549bcc17d0
syedanwarshah <syed.anwar@nexuscorp-ltd.com>
2021-04-21 00:21:48 -0700

    hotels in progress

--- hotels/app/Hotel.php
+++ hotels/app/Hotel.php
@@ -3,8 +3,12 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
-
+use App\Room;
+ 
 class Hotel extends Model
 {
-    //
+    public function rooms()
+    {
+        return $this->hasMany('App\Room');
+    }
 }
---
+++ hotels/database/migrations/2021_04_21_064325_create_rooms_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRoomsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('rooms', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name')->nullable();
+            $table->text('description')->nullable();
+            $table->unsignedBigInteger('hotel_id')->nullable();
+            $table->foreign('hotel_id')->references('id')->on('hotels')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('rooms');
+    }
+}
