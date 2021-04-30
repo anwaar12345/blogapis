b32029198954c39f72f0bbde2f88953d2c65e88f
syedanwarshah <syed.anwar@nexuscorp-ltd.com>
2021-04-30 03:51:55 -0700

    hotels

--- hotels/app/Room.php
+++ hotels/app/Room.php
@@ -3,8 +3,11 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
-
+use App\Room;
 class Room extends Model
 {
-    //
+    public function type()
+    {
+        $this->belongsTo('App\RoomType');
+    }
 }
